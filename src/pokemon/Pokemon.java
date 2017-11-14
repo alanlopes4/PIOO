98cd9653542eee8ec3f6d80235c477dbffbb8f06
Alan Lopes <alan@alanlopes4@gmail.com>
2017-11-14 16:27:15 -0200

    Adicionando Getters e Setters

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pokemon;
 
-/**
- *
- * @author sylar
- */
+
 public class Pokemon {
 
   private int level;
@@ -30,5 +22,127 @@ public class Pokemon {
   public double valorAtributo(){
       return 0.0;
   }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getHpAtual() {
+        return hpAtual;
+    }
+
+    public void setHpAtual(double hpAtual) {
+        this.hpAtual = hpAtual;
+    }
+
+    public double getHpMax() {
+        return hpMax;
+    }
+
+    public void setHpMax(double hpMax) {
+        this.hpMax = hpMax;
+    }
+
+    public double getAtk() {
+        return atk;
+    }
+
+    public void setAtk(double atk) {
+        this.atk = atk;
+    }
+
+    public double getDef() {
+        return def;
+    }
+
+    public void setDef(double def) {
+        this.def = def;
+    }
+
+    public double getSpe() {
+        return spe;
+    }
+
+    public void setSpe(double spe) {
+        this.spe = spe;
+    }
+
+    public double getSpd() {
+        return spd;
+    }
+
+    public void setSpd(double spd) {
+        this.spd = spd;
+    }
+
+    public int getModifierAccuracy() {
+        return modifierAccuracy;
+    }
+
+    public void setModifierAccuracy(int modifierAccuracy) {
+        this.modifierAccuracy = modifierAccuracy;
+    }
+
+    public int getModifierEvasion() {
+        return modifierEvasion;
+    }
+
+    public void setModifierEvasion(int modifierEvasion) {
+        this.modifierEvasion = modifierEvasion;
+    }
+
+    public int getModifierAtk() {
+        return modifierAtk;
+    }
+
+    public void setModifierAtk(int modifierAtk) {
+        this.modifierAtk = modifierAtk;
+    }
+
+    public int getModifierDef() {
+        return modifierDef;
+    }
+
+    public void setModifierDef(int modifierDef) {
+        this.modifierDef = modifierDef;
+    }
+
+    public int getModifierSpe() {
+        return modifierSpe;
+    }
+
+    public void setModifierSpe(int modifierSpe) {
+        this.modifierSpe = modifierSpe;
+    }
+
+    public int getModifierSpd() {
+        return modifierSpd;
+    }
+
+    public void setModifierSpd(int modifierSpd) {
+        this.modifierSpd = modifierSpd;
+    }
+
+    public boolean isConfusuion() {
+        return confusuion;
+    }
+
+    public void setConfusuion(boolean confusuion) {
+        this.confusuion = confusuion;
+    }
+
+    public boolean isFlinch() {
+        return flinch;
+    }
+
+    public void setFlinch(boolean flinch) {
+        this.flinch = flinch;
+    }
+  
+  
     
 }
