fab7166150880ebd2d2454a4d58dc5887478f821
ALISSON <ALISSON@ALISSON-PC>
2017-12-09 13:18:32 -0200

    Atualização: Correção bug da escolha

--- src/pokemon/Humano.java
+++ src/pokemon/Humano.java
@@ -22,7 +22,7 @@ public class Humano extends Jogador{
             System.out.println("1 - Troca e 2 - Ataque?");
             Scanner opcao = new Scanner(System.in);
             escolha = opcao.nextInt();
-            if(super.getTime().size()<=1)
+            if(super.getTime().size()<=1 && escolha == 1)
                 System.out.println("Não é possível trocar, possui apenas 1 pokemon");
         }while(super.getTime().size() <=1 && escolha == 1);
         super.setComandoEscolhido(escolha);
